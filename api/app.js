e6a0bb0e56cff2c6d5873937dbad123b50e8ce60
Santiago Cortés <david.cortes@correounivalle.edu.co>
2021-04-09 18:00:43 -0500

    Creación middleware.

--- api/app.js
+++ api/app.js
@@ -4,9 +4,12 @@
  * no podría funcionar
  */
 const typeorm = require('typeorm')
-const tipoDoc = require('./entity/TipoDoc')
 const express = require('express')
+require('express-async-errors')
 const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 // Esta función se ejecuta inmediatamente.
 const foo = (async () => {
@@ -15,40 +18,12 @@ const foo = (async () => {
     // Solo después de crear la conexión se puede importar 
     // el control de las entidades
     const control = require('./controllers/Control')
-    
-    /*
-     * Ejemplos del uso de control
-     */
-
-    // Traer todos los registros de la entidad tipoDoc
-    const result = await control.getAll(tipoDoc)
-    console.log(result)
-
-    // Insertar registro nuevo
-    //const nuevoDoc = {
-    //    name: 'Libreta militar'
-    //}
-    //const insertResult = await control.insert(tipoDoc, nuevoDoc)
-    console.log(insertResult)
-
-    
-    const todos = await control.getField(tipoDoc, "name") 
-
-    console.log(todos)
-
-    const Like = typeorm.Like
-    const nombreEmpiezaConC = await typeorm.getRepository(tipoDoc)
-        .find({ name: Like('C%')})
-
-    console.log(nombreEmpiezaConC)
-
-    const primerRegistro = await typeorm.getRepository(tipoDoc)
-        .findOne(1)
-
-    console.log(primerRegistro)
-
     await conn.close()
-    console.log('Conexión terminada')
+    
 })()
+app.use(cors())
+app.use(express.json())
+
+app.use(middleware.requestLogger)
 
 module.exports = app
