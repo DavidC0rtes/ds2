ca1aa2ee902518a75b259aed184f794e30d9a155
Santiago Cortés <david.cortes@correounivalle.edu.co>
2021-04-16 09:43:35 -0500

    Documentacion minima.

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,9 @@ import NavBar from './components/Navbar'
 import Login from './pages/Login'
 import SignUp from './pages/SingUp'
 
+// Estas cosas nos permiten redirigir
+// a las diferentes vistas cuando
+// se le da clic a algún enlace.
 import { 
   BrowserRouter as Router,
   Switch,
@@ -14,9 +17,9 @@ function App() {
   return (
       <div>
         <Container maxWidth="md">
-        <Router>
+        <Router> {/* <-- importante que el Router este acá*/}
             <NavBar />
-            <Switch>
+            <Switch>  {/*<-- acá se definen todas las rutas y hacia que componente redirigen*/}
                 <Route exact path="/"/>
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/registrarse" component={SignUp} />
