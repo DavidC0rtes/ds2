6379bfd44550cc6b7752df8b77390fb87f878e00
Santiago Cortés <david.cortes@correounivalle.edu.co>
2021-04-21 12:25:49 -0500

    Primer avance, token y login funcionando.

---
+++ api/routes/login.js
@@ -0,0 +1,47 @@
+/* Este endpoint se encarga del Log In de usuarios.
+ * Cada usuario autenticado recibe un token el cual es un hash
+ * que se calcula con su contraseña y una cadena 'aleatoria'.
+ * Este token se utiliza para asegurarse que el usuario es quien dice 
+ * ser en las demás partes del proyecto.
+*/
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('../entity/Usuarios')
+const control = require('../controllers/Control')
+const loginRouter = require('express').Router()
+
+/* Un login se entiende como una solicitud POST a api/login.
+ * En el cuerpo de la solicitud debe estar el correo y la contraseña del usuario.
+ *
+ */
+loginRouter.post('/', async (request, response) => {
+    const body = request.body
+    
+    // Se trae al usuario con el email proporcionado desde la BD
+    const user = await control.getBy(User, 'email', body.email)
+    
+    // Se comprueba si el hash en la bd es el mismo que el hash
+    // calculado a partir de la contraseña ingresada.
+    const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(body.password, user.password)
+
+    if (!(user && passwordCorrect)) {
+        return response.status(401).json({
+            error: 'correo electrónico y/o contraseña inválidos'
+        })
+    }
+
+    const userForToken = {
+        email: user.email,
+        id: user.id
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET)
+
+    response
+        .status(200)
+        .send({ token, email: user.email })
+})
+
+module.exports = loginRouter
