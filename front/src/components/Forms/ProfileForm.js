2b3c19d8b8bd0567f823b8c369273346210e1070
Santiago Cortés <david.cortes@correounivalle.edu.co>
2021-08-30 09:46:21 -0500

    Solución de algunos warnings de eslint

--- front/src/components/Forms/ProfileForm.js
+++ front/src/components/Forms/ProfileForm.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
-import { Typography } from '@material-ui/core'
 
 import FormHandler from '../../variables/formHandler'
 import userService from '../../services/users'
