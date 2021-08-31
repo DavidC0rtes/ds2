8441a92e114262ca03fc704886aa44e0ffde4245
JoseAHC-UV <66497674+JoseAHC-UV@users.noreply.github.com>
2021-08-31 17:37:48 -0500

    Update Menu.js

---
+++ front/src/components/Menu.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import FormControl from '@material-ui/core/FormControl';
