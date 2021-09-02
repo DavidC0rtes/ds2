b019816803da3d6d37a45199cf190c47f006a58a
alejandro1403 <orozco.alejandro@correounivalle.edu.co>
2021-09-02 02:10:36 -0500

    Menu combinado con categoria

---
+++ api/routes/products.js
@@ -0,0 +1,59 @@
+const productsRouter = require('express').Router()
+const control = require('../controllers/Control')
+const product = require('../entity/Productos')
+const category = require('../entity/Categorias')
+
+
+
+//Confirmar que existe la categoria asignada al producto
+// Determina si el usuario con correo email existe. No devuelve al usuario 
+productsRouter.head('/:categoria', async (request, response) => {
+    const result = await control.getBy(product, 'categoria', request.params.categoria)
+
+    if (result) {
+        return response.status(204).end()
+    }
+
+    return response.status(404).end()
+})
+
+/*Creación de un nuevo producto
+* La estructura es:
+* "nombre": "ejemplo",
+* "descripcion": "aaa"
+* "cantidad": "2"
+* "iva": "1.19"
+* "precio": "4000"
+* "id_categoria": "1"
+* Solo descripción, cantidad y iva pueden dejarse vacíos.
+* Note que debe existir una categoría antes de agregar un producto 
+*/
+
+//Retornar producto de una categoria dada
+
+productsRouter.get('/:categoria', async (request, response) => {
+    const categoryproducts = await control.getBy(product, 'categoria', request.params.categoria)
+    response.json(categoryproducts)
+})
+productsRouter.get('/', async (request, response) => {
+    const products = await control.getAll(product)
+    response.json(products)
+})
+
+productsRouter.post('/', async (request, response) => {
+    const body = request.body     
+    const newProduct = {
+        nombre: body.nombre,
+        descripcion: body.descripcion,
+        cantidad: body.cantidad,
+        iva: body.iva,
+        precio: body.precio,
+        categoria: body.categoria,
+        imagen: body.imagen
+    } 
+    const insertedProduct = await control.insert(product, newProduct)
+    
+    response.json(insertedProduct)
+})
+
+module.exports = productsRouter
--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+<<<<<<< Updated upstream
 function App() {
   return (
     <div className="App">
@@ -20,6 +21,41 @@ function App() {
       </header>
     </div>
   );
+=======
+// Layouts
+import Admin from "./layouts/Admin";
+import Login from './views/Login/Login'
+import SignUp from './views/Login/SignUp'
+import Categories from './views/Categoria/Categoria'
+import Productos from './views/Producto/Productos'
+import Perfil from './views/Usuario/Perfil.js'
+import Inicio from './layouts/Inicio'
+
+
+import "assets/css/material-dashboard-react.css?v=1.9.0";
+const hist = createBrowserHistory()
+
+const App = () => { 
+        return (
+            <ProvideAuth>
+            <Router history={hist}>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/" component={Inicio}/>
+                    <Route path="/admin" component={Admin} />
+                    <Route path="/perfil" component={Perfil}/>
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/registrarse" component={SignUp} />
+                    <Route exact path="/categorias" component={Categories} />
+                    <Redirect exact from = "/admin" to= "/admin/dashboard"/>
+                    <Route exact path="/menu" component={Productos} />
+                    <Redirect from="/admin" to="/admin/dashboard" />
+
+                </Switch>
+            </Router>
+            </ProvideAuth>
+        );
+>>>>>>> Stashed changes
 }
 
 export default App;
