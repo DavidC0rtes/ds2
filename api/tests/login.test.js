ed841bb7c7dda2abad43e660e943e2153b1dbf87
Santiago Cortés <david.cortes@correounivalle.edu.co>
2021-09-15 21:47:49 -0500

    Prueba de caminos para el login

--- api/tests/login.test.js
+++ api/tests/login.test.js
@@ -1,5 +1,4 @@
 const request = require('supertest');
-const express = require('express');
 
 const app = require('../app');
 const api = request(app)
@@ -7,8 +6,7 @@ const api = request(app)
 
 describe('El servidor retorna 401 cuando', () => {
 
-    test('El usuario no existe', () => {
-
+    test('el usuario no existe', () => {
         api
         .post('/api/login')
         .set('Content-Type', 'application/json/')
@@ -18,6 +16,46 @@ describe('El servidor retorna 401 cuando', () => {
             if (err) throw err;
         });
     })
+    
+    test('el usuario no está activo', () => {
+      api
+        .post('/api/login')
+        .set('Content-Type', 'application/json/')
+        .send({email: 'pepito'})
+        .expect(401)
+        .end(function(err, res) {
+            if (err) throw err;
+        });
+    })
+
+    test('la contraseña es incorrecta', () => {
+        api
+        .post('/api/login')
+        .set('Content-Type', 'application/json/')
+        .send({email: 'admin', password: 'esta no es la contraseña'})
+        .expect(401)
+        .end(function(err, res) {
+            console.log(res)
+            if (err) throw err;
+        });
+    })
 })
 
 
+describe('El servidor devuelve retorna 200 cuando', () => {
+    test(' el correo y la contraseña son correctos', () => {
+        
+        api
+        .post('/api/login')
+        .set('Content-Type', 'application/json/')
+        .send({email: 'admin', password: 'admin'})
+        .expect(200)
+        .end(function(err, res) {
+            if (err) throw err;
+        });
+    })
+})
+
+afterAll(async () => {
+	await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
+});
